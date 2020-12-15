290255509374d8effc14091500256a7e107d3e5a
angelmazur <stanlpa3.0@gmail.com>
2020-12-15 18:14:55 +0100

    agregado clasde de alcance de funciones e inicializado npm

---
+++ clase5.js
@@ -0,0 +1,14 @@
+let nombre = 'Angel';
+
+function imprimirNombreMayusculas () {
+    nombre = nombre.toUpperCase ()
+    console.log (nombre)
+};
+imprimirNombreMayusculas();
+
+function imprimirNombreEnMayusculas1(n) {
+    n = n.toLowerCase()
+    console.log(n)
+}
+
+imprimirNombreEnMayusculas1(nombre)
