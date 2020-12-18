e3849f01b022c429b456c160a29b12d600a00386
angelmazur <stanlpa3.0@gmail.com>
2020-12-18 21:13:06 +0100

    agregado clase de dowhile

--- clase12 - loop while.js
+++ clase12 - loop while.js
@@ -21,6 +21,7 @@ const comeMucho = () => Math.random() < 0.3;
 const haceDeporte = () => Math.random() < 0.4;
 
 let dias = 0;
+//Comienza en 0 y el código se ejecutara  e incrementaremos en 1 la cantidad de días
 
 while (angel.peso > META){
     //el código que esta entre llaves se va a repetir siempre que la condición que este entre parentesis sea true
@@ -32,7 +33,8 @@ while (angel.peso > META){
         //perderPeso
         perderPeso(angel)
     }
-    dias += 1
+    dias += 1 //incremento de 1 cuando el código sea true
 };
-//Mientras que la condición sea verdadera el ciclo se va a repetir, cuando de false se ejecutara el resto del código
+//Mientras que la condición sea verdadera el ciclo se va a repetir e irá aumentando los días, cuando de false se ejecutara el resto del código
 console.log(`Pasan ${dias} días hasta que ${angel.nombre} pierde 3 kg`)
+
